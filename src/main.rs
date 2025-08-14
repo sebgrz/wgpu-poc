6d3f51e7a7ce5f3439dfbb6810d12480fcc9c0e2
Seba G <sebag.git@gmail.com>
2025-08-14 19:19:15 +0200

    winit window

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::rc::Rc;
+
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, EventLoop},
+    window::{Window, WindowId},
+};
+
+#[derive(Default)]
+struct App {
+    window: Option<Rc<Window>>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = Rc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+
+        self.window = Some(window.clone());
+        window.request_redraw();
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+    let mut app = App::default();
+
+    event_loop.run_app(&mut app).unwrap();
+}
